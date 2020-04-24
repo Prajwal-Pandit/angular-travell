33c899613bd7149885ef0b89bc857a07c6efb1dd
Sushant <prajwal.pandit7@gmail.com>
2020-04-24 12:06:25 +0530

    initial commit

---
+++ src/app/body/body.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.scss'],
+})
+export class BodyComponent {
+  @Input() deviceXs: boolean;
+  nepalImg =
+    'https://media.tacdn.com/media/attractions-splice-spp-674x446/07/87/d3/92.jpg';
+  tokyoImg =
+    'https://stillmed.olympic.org/media/Images/OlympicOrg/News/2020/03/30/2020-03-30-tokyo-thumbnail-02.jpg';
+  sanImg =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTU9ANS5rcieo7OUFeyjBTATWos2H9NjOmH1w2QHgDnCpKsiM_0&usqp=CAU';
+  lonImg =
+    'https://secure.i.telegraph.co.uk/multimedia/archive/02423/london_2423609b.jpg';
+  singImg =
+    'https://ttgasia.2017.ttgasia.com/wp-content/uploads/sites/2/2020/04/Singapore-merlion.jpg';
+  ausImg = 'https://churific.com/wp-content/uploads/2015/06/sydney-image.jpg';
+  canImg =
+    'https://assets.simpleviewinc.com/simpleview/image/upload/c_limit,h_1200,q_75,w_1200/v1/clients/vancouverbc/Aerial_Sunset_Vancouver_d3_copy_1bb86ed0-1edc-4cda-841d-0b033ca0bb72.jpg';
+  newImg =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS9ufVG-g9uuyW6N2vbrF-VaOukwNF4KSXqO_Q0P4fnVJz0EO4l&usqp=CAU';
+}
