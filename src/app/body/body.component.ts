e81afb985a03ec44456df9f9cf7a5485bc082b91
Sushant <prajwal.pandit7@gmail.com>
2020-04-26 14:42:22 +0530

    places

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { DataService } from '../data.service';
-import { FlexOrderStyleBuilder } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-body',
@@ -10,33 +9,15 @@ import { FlexOrderStyleBuilder } from '@angular/flex-layout';
 export class BodyComponent implements OnInit {
   @Input() deviceXs: boolean;
 
-  public images = [];
+  public images1 = [];
+  public images2 = [];
+  public button = [];
 
-  // nepalImg =
-  //   'https://stillmed.olympic.org/media/Images/OlympicOrg/News/2020/03/30/2020-03-30-tokyo-thumbnail-02.jpg';
-
-  // tokyoImg =
-  //   'https://stillmed.olympic.org/media/Images/OlympicOrg/News/2020/03/30/2020-03-30-tokyo-thumbnail-02.jpg';
-
-  // sanImg =
-  //   'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTU9ANS5rcieo7OUFeyjBTATWos2H9NjOmH1w2QHgDnCpKsiM_0&usqp=CAU';
-
-  // lonImg =
-  //   'https://secure.i.telegraph.co.uk/multimedia/archive/02423/london_2423609b.jpg';
-
-  // singImg =
-  //   'https://ttgasia.2017.ttgasia.com/wp-content/uploads/sites/2/2020/04/Singapore-merlion.jpg';
-
-  // ausImg = 'https://churific.com/wp-content/uploads/2015/06/sydney-image.jpg';
-
-  // canImg =
-  //   'https://assets.simpleviewinc.com/simpleview/image/upload/c_limit,h_1200,q_75,w_1200/v1/clients/vancouverbc/Aerial_Sunset_Vancouver_d3_copy_1bb86ed0-1edc-4cda-841d-0b033ca0bb72.jpg';
-
-  // newImg =
-  //   'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS9ufVG-g9uuyW6N2vbrF-VaOukwNF4KSXqO_Q0P4fnVJz0EO4l&usqp=CAU';
-
+  
   constructor(private data: DataService) {
-    this.images = this.data.getdata();
+    this.images1 = this.data.getdataf();
+    this.images2 = this.data.getdatas();
+    this.button = this.data.getbutton();
   }
   ngOnInit() {}
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class DataService {
   constructor() {}
 
-  public getdata() {
+  public getdataf() {
     return [
       {
         image:
@@ -17,49 +17,66 @@ export class DataService {
       },
       {
         image:
+          'https://stillmed.olympic.org/media/Images/OlympicOrg/News/2020/03/30/2020-03-30-tokyo-thumbnail-02.jpg',
+        name: 'Tokyo',
+        place: 'Nepal',
+        desc: 'This is the place of Nepal',
+      },
+      {
+        image:
           'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTU9ANS5rcieo7OUFeyjBTATWos2H9NjOmH1w2QHgDnCpKsiM_0&usqp=CAU',
-        name: 'Pokhara',
+        name: 'San-Francisco',
         place: 'Nepal',
         desc: 'This is the place of Nepal',
       },
       {
         image:
-          'https://stillmed.olympic.org/media/Images/OlympicOrg/News/2020/03/30/2020-03-30-tokyo-thumbnail-02.jpg',
-        name: 'KATHMANDU',
-        place: 'Nepal',
-        desc: 'This is the place of Nepal',
-      },
-      {
-        image:
-          'https://stillmed.olympic.org/media/Images/OlympicOrg/News/2020/03/30/2020-03-30-tokyo-thumbnail-02.jpg',
-        name: 'KATHMANDU',
-        place: 'Nepal',
-        desc: 'This is the place of Nepal',
-      },
-      {
-        image:
-          'https://stillmed.olympic.org/media/Images/OlympicOrg/News/2020/03/30/2020-03-30-tokyo-thumbnail-02.jpg',
-        name: 'KATHMANDU',
-        place: 'Nepal',
-        desc: 'This is the place of Nepal',
-      },
-      {
-        image:
-          'https://stillmed.olympic.org/media/Images/OlympicOrg/News/2020/03/30/2020-03-30-tokyo-thumbnail-02.jpg',
-        name: 'KATHMANDU',
+          'https://secure.i.telegraph.co.uk/multimedia/archive/02423/london_2423609b.jpg',
+        name: 'London',
         place: 'Nepal',
         desc: 'This is the place of Nepal',
       },
     ];
   }
+
+  public getdatas() {
+    return [
+      {
+        image:
+          'https://ttgasia.2017.ttgasia.com/wp-content/uploads/sites/2/2020/04/Singapore-merlion.jpg',
+        name: 'Singapore',
+        place: 'Nepal',
+        desc: 'This is the place of Nepal',
+      },
+      {
+        image:
+          'https://churific.com/wp-content/uploads/2015/06/sydney-image.jpg',
+        name: 'Australia',
+        place: 'Nepal',
+        desc: 'This is the place of Nepal',
+      },
+      {
+        image:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS9ufVG-g9uuyW6N2vbrF-VaOukwNF4KSXqO_Q0P4fnVJz0EO4l&usqp=CAU',
+        name: 'New-York',
+        place: 'New-York',
+        desc: 'This is the place of Nepal',
+      },
+      {
+        image:
+          'https://assets.simpleviewinc.com/simpleview/image/upload/c_limit,h_1200,q_75,w_1200/v1/clients/vancouverbc/Aerial_Sunset_Vancouver_d3_copy_1bb86ed0-1edc-4cda-841d-0b033ca0bb72.jpg',
+        name: 'Canada',
+        place: 'Nepal',
+        desc: 'This is the place of Nepal',
+      },
+    ];
+  }
+
+  public getbutton() {
+    return [
+      { text: 'Flights', icon: 'flight' },
+      { text: 'Hotels', icon: 'hotel' },
+      { text: 'Car-Hire', icon: 'directions_car' },
+    ];
+  }
 }
-
-// 'https://secure.i.telegraph.co.uk/multimedia/archive/02423/london_2423609b.jpg',
-
-// 'https://ttgasia.2017.ttgasia.com/wp-content/uploads/sites/2/2020/04/Singapore-merlion.jpg',
-
-// 'https://churific.com/wp-content/uploads/2015/06/sydney-image.jpg',
-
-// 'https://assets.simpleviewinc.com/simpleview/image/upload/c_limit,h_1200,q_75,w_1200/v1/clients/vancouverbc/Aerial_Sunset_Vancouver_d3_copy_1bb86ed0-1edc-4cda-841d-0b033ca0bb72.jpg',
-
-// 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS9ufVG-g9uuyW6N2vbrF-VaOukwNF4KSXqO_Q0P4fnVJz0EO4l&usqp=CAU',
